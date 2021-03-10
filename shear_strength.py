bef2f6d080705e2249c7a68fc7a2b14eb4d0a84b
Emma Swaninger <eswaninger@kennicott.ibest.uidaho.edu>
2021-03-10 11:54:29 -0800

    shear_strength.py

--- shear_strength.py
+++ shear_strength.py
@@ -39,29 +39,27 @@ unsur = sur_block.ravel()                            #list of surface elevations
 # Hard Force Balance Variables
 
 L = 1340                                             #Length of iceberg perpendicular to flow (m)
-W =  340                                             #Width of iceberg at tooth parallel to flow (m)
+W_t = 340                                             #Width of iceberg at tooth parallel to flow (m)
 W_s = 230                                            #Width of iceberg at rotating shadow in MLI(m)
 pw = 1020                                            #density of water (kg/m**2)
 pi = 920                                             #density of ice (kg/m**2)
 g = 9.81                                             #gravitational acceleration (m/s**2)
 
-H= unice                                             #estimated iceberg heights from BedMachine
-E = W/H                                              #iceberg aspect ratio
-
-haf= unsur - H*(1- (pi/pw))                          #height above/below floatation (m)
+H= np.arange(450, 700, 10)                           #estimated iceberg heights from BedMachine (m)
+E = W_s/H                                              #iceberg aspect ratio
 
 # Angles on glacier REFER TO IDEAL ICEBERG IN THESIS (FIG. 8)
 
-geoid_H = 31.657                                     #m
+geoid_H = 31.657                                     # geoid height at rink (m)
 
 #H = np.arange(450, 700, 10)
 
-a = 427                                              #TRI elevation
-b = 63 - geoid_H                                     #upglacier elevation
-c = 54 - geoid_H                                     #arcticdem terminus elevation
+a = 427                                              #TRI elevation (m)
+b = 63 - geoid_H                                     #upglacier elevation (m)
+c = 54 - geoid_H                                     #arcticdem terminus elevation (m)
 
-ab = 5700                                            #distance between TRI and glacier 
-bc = 75                                              #horizontal distance between c and b
+ab = 5700                                            #distance between TRI and glacier (m)
+bc = 75                                              #horizontal distance between c and b (m)
 
 alpha = math.atan((a-b)/ab)                          #angle b/w TRI and horizontal
 alpha = np.rad2deg(alpha)
@@ -78,63 +76,51 @@ zeta = np.rad2deg(math.acos(B/W_s))                 #angle complimentary to lowe
 theta = 180 - y - zeta                              # Rotation angle of iceberg 
 psi = theta - beta                                  #angle between horizontal seal level and iceberg rotating                                            
 
-#Additional Force Variables (Volume, Area, Mass)
+#Additional Force Variables (Iceberg lengths, Volume, Area, Mass)
 
-theta = np.deg2rad(theta)
+C1 = math.tan(np.deg2rad(theta))*(W_s)              #length of iceberg facing the ocean tipping out of the water (Fig 8 - C)
+C2 = math.tan(np.deg2rad(psi))*(W_s)                #length of iceberg used to separate the aerial portions of iceberg (Fig 8 - C)
+C3 = C1-C2                                          #inner length of the aerial portion of iceberg
 
-C1 = math.tan(np.deg2rad(theta))*(W_s)
-C2 = math.tan(np.deg2rad(psi))*(W_s)
-C3 = C1-C2
-
-vol_list = []                                        #whole iceberg estimated volume
-for i in H:
-    vol = L*W*i
-    vol_list.append(vol)
-    
-A=[]                                                #area of idealized iceberg
-for i in H:
-    a = (W_s/i)*(i**2)
-    A.append(a)
-A = np.array(A)   
  
-A= H*W_s     
+A= H*W_s                                            #area of idealized iceberg face (Fig 8)
 A1 = (1/2)*C2*W_s                                   #area of triangle rotated above ocean surface            
 A2 = (C3)*W_s                                       #area of rectangle rotated above ocean surface
 Vi = L*W_s*H                                        #volume of ice with different heights
 Va = A1*L + A2*L                                    #volume of iceberg above water
 Vs = Vi - Va                                        #volume of submerged ice with different heights
 
-m = Vi*pi                                            #mass of iceberg
+m = Vi*pi                                           #mass of iceberg
 ms= Vs*pw                                           #mass of submerged portion of iceberg
 
-Fg = -pi*g*Vi                                      #force of gravity for different masses of icebergs
+Fg = -pi*g*Vi                                       #force of gravity for different masses of icebergs
 Fb = pw*g*Vs                                        #force of buoyancy for different masses of icebergs
 
-Fnet = -(Fg + Fb)                                      #net force on iceberg
+Fnet = -(Fg + Fb)                                   #net force on iceberg
 
-Pmax = Fnet/A [1]                                      #shear strength (Pa)
-Pmax = Pmax/1000                                    # kPa
+Pmax = Fnet/A[1]                                    #shear strength (Pa)
+Pmax = Pmax/1000                                    # (kPa)
 
 # Plot shear strength b/w tooth and 3rd iceberg
 
 plt.figure()
-plt.plot(A, Pmax,  '.')
+plt.plot(H, Pmax,  '.')
 plt.grid()
 
 plt.ylabel("Shear Strength (kPa)")
-plt.xlabel('area (m**2)')
+plt.xlabel('Terminus thickness (m)')
 
 plt.gcf().autofmt_xdate()
 
 # Torque moment due to buoyancy
 
-C  = -H*np.cos(theta)*((pi/pw)-(1/2))           #iceberg's center of mass
-C1 = -(1/3)*(E)*H*np.sin(theta)                 #center of mass within triangle
-C2 = -(H/2)*np.cos(theta)*((pi/pw)+(1/2)*E*(np.tan(theta))) #center of mass of the rectangle
-Cs = (C1*A1 + C2*A2)/(pi/pw)*A                  #Center of Buoyancy
+cm  = -H*np.cos(theta)*((pi/pw)-(1/2))           #iceberg's center of mass
+cm1 = -(1/3)*(E)*H*np.sin(theta)                 #center of mass within triangle
+cm2 = -(H/2)*np.cos(theta)*((pi/pw)+(1/2)*E*(np.tan(theta))) #center of mass of the rectangle
+cm_s = (C1*A1 + C2*A2)/(pi/pw)*A                  #Center of Buoyancy
 
 
-T = g*pi*L*A*(Cs-C)
+T = g*pi*L*A*(cm_s-cm)
 
 plt.figure()
 plt.plot(T, E,  '.')
@@ -144,45 +130,3 @@ plt.ylabel('Aspect Ratio')
 
 plt.gcf().autofmt_xdate()
 
-#%%
-plt.figure()
-plt.plot(C2,Cs)
-
-fig, axes = plt.subplots(nrows= 4, clear = True)
-y = np.arange(0,60, 1)
-axes[0].plot(theta, Pmax, '.', color='blue', markersize = 3.5)
-#axes[0,0].plot(np.unique(area), np.poly1d(np.polyfit(area, P, 2))(np.unique(P)))
-axes[0].set_ylabel('Shear Strength (kPa)', fontsize = 12)
-axes[0].yaxis.set_label_coords(-0.09,0.5)
-axes[0].tick_params(axis='x', which='major', labelsize=12)
-axes[0].tick_params(axis='y', which='major', labelsize=12)
-axes[0].grid() 
-
-
-#axes[1].plot(theta,Sxx,'.', color='blue',markersize = 3.5)
-#axes[1].set_ylabel('Sxx (kPa)', fontsize = 12)
-#axes[1].yaxis.set_label_coords(-0.09,0.5)
-#axes[1].tick_params(axis='x', which='major', labelsize=12)
-#axes[1].tick_params(axis='y', which='major', labelsize=12)
-#axes[1].grid() 
-#axes[1].axvspan(0.75,0.85, alpha= 0.2, color = 'gray')
-
-axes[2].plot(theta,Fnet, '.',color='blue', markersize =3.5)
-axes[2].set_ylabel('Net-Force (N)', fontsize = 12)
-axes[2].yaxis.set_label_coords(-0.09,0.5)
-axes[2].tick_params(axis='x', which='major', labelsize=12)
-axes[2].tick_params(axis='y', which='major', labelsize=12)
-axes[2].grid() 
-
-
-axes[3].plot(theta,T, '.',color='blue', markersize =3.5)
-axes[3].set_ylabel('Torque Moment (Nm)', fontsize = 12)
-axes[3].yaxis.set_label_coords(-0.09,0.5)
-axes[3].tick_params(axis='x', which='major', labelsize=12)
-axes[3].tick_params(axis='y', which='major', labelsize=12)
-axes[3].grid() 
-axes[3].axis('auto')
-axes[3].set_xlabel("Aspect Ratio", fontsize=12)
-
-
-plt.gcf().autofmt_xdate()
